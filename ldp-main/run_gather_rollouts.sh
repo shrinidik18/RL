9706b8a85cc5eed3a5220df5d2fafa410840d804
shrinidik18 <shrinidikuppurajan@gmail.com>
2026-08-03 00:07:24 +0530

    Delete ldp-main directory

--- ldp-main/run_gather_rollouts.sh
+++
@@ -1,8 +0,0 @@
-checkpoint=$2
-name=$1
-
-if [ -n "$3" ]; then
-    python gather_rollouts.py -c $2 -n 400 -o "rollouts/$1" -e $3
-else
-    python gather_rollouts.py -c $2 -n 400 -o "rollouts/$1"
-fi
